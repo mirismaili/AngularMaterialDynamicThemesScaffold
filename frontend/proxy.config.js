8865b7b4b34ec04fab665a16f601165365638f86
Unknown <fedegur@gmail.com>
2019-06-15 07:49:45 +0200

    proxy added for server requests

---
+++ frontend/proxy.config.js
@@ -0,0 +1,13 @@
+const PROXY_CONFIG = [
+  {
+    context: [
+      '/api',
+    ],
+    target: 'http://localhost:3000',
+    secure: false,
+    logLevel: 'debug',
+    changeOrigin: true
+  }
+]
+
+module.exports = PROXY_CONFIG;
