3d8f48e82bcc7b566bf11621ec198f83d77e1bb7
Unknown <fedegur@gmail.com>
2019-06-15 10:31:58 +0200

    minor fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   app_base_url: 'http://0.0.0.0:4200',
   app_api_service_url: 'http://localhost:3000',
   app_google_analytics_id: '',
-  app_cookieDomain: '',
+  app_cookie_domain: '',
 };
