db8df230873ecac8d8f17ba2f875061197b58666
Unknown <fedegur@gmail.com>
2019-06-15 08:47:01 +0200

    lazy loading routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+  // tslint:disable:max-line-length
+// core
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// config
+import { APP } from './config';
+// pages
+import { HomeComponent } from './pages';
 
-const routes: Routes = [];
+
+// routes (use lazy load when needed)
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: APP.paths.sample, loadChildren: () => import('./pages/sample/sample.module').then(({ SampleModule }) => SampleModule) },
+  { path: '**', loadChildren: () => import('./pages/notfound/notfound.module').then(({ NotfoundModule }) => NotfoundModule), data: { path: '/', pathName: 'Go back' } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
