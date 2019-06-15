41b554f2cad30392f82e8070e44f095741a90bac
Unknown <fedegur@gmail.com>
2019-06-15 10:27:19 +0200

    minor changes

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'Angular 8 + TypeScript Scaffold';
+  title = 'Angular 8 + TypeScript';
 
   constructor() { }
 
