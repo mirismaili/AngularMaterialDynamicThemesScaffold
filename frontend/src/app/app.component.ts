256d6cbfd4caec2b829c265e9cf8a0813e949dd3
Unknown <fedegur@gmail.com>
2019-06-15 08:47:23 +0200

    app component modified

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular 8 + TypeScript Scaffold';
-}
+export class AppComponent {}
