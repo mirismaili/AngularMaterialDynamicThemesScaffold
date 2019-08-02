8ef75e37b4eda19ea2c44f65ab5f1fcd3a163653
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-08-02 12:07:04 +0430

    refactor: First implementation of Dynamic-Themes ✓
    First implementation of Dynamic-Themes until step 5 ("5. Run your app") of this manual:
    https://medium.com/@s.m.mirismaili/angular-material-dynamic-themes-compatible-with-angular-7-8-e642ad3c09f4

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 // core
-import { Component } from '@angular/core';
-import { RouterOutlet, ActivatedRoute } from '@angular/router';
+import {Component, HostBinding} from '@angular/core';
+import {RouterOutlet, ActivatedRoute} from '@angular/router';
+import {OverlayContainer} from '@angular/cdk/overlay'
+
 // packages
 import Koji from 'koji-tools';
 // animations
-import { routeAnimations } from './animations';
+import {routeAnimations} from './animations';
 
+const THEME_DARKNESS_SUFFIX = `-dark`
 
 @Component({
   selector: 'app-root',
@@ -14,11 +17,38 @@ import { routeAnimations } from './animations';
   animations: [routeAnimations]
 })
 export class AppComponent {
-
-  constructor() {
+  @HostBinding('class') activeThemeCssClass: string
+  isThemeDark = false
+  activeTheme: string
+  
+  constructor(private overlayContainer: OverlayContainer) {
     Koji.pageLoad();
+    
+    // Set default theme here:
+    this.setActiveTheme('deeppurple-amber', /* darkness: */ false)
   }
-
+  
+  setActiveTheme(theme: string, darkness: boolean = null) {
+    if (darkness === null)
+      darkness = this.isThemeDark
+    else if (this.isThemeDark === darkness) {
+      if (this.activeTheme === theme) return
+    } else
+      this.isThemeDark = darkness
+    
+    this.activeTheme = theme
+    
+    const cssClass = darkness === true ? theme + THEME_DARKNESS_SUFFIX : theme
+    
+    const classList = this.overlayContainer.getContainerElement().classList
+    if (classList.contains(this.activeThemeCssClass))
+      classList.replace(this.activeThemeCssClass, cssClass)
+    else
+      classList.add(cssClass)
+    
+    this.activeThemeCssClass = cssClass
+  }
+  
   /**
    * Returns the the current active route to control the animation to apply
    * @param outlet: RouterOutlet
